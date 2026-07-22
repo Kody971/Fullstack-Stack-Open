4420a9cc77f1687a56c2ea2cc03fe8ee316dfafa
Ayub Khoirudin <khoirudinayub971@gmail.com>
2026-07-22 14:47:01 +0700

    Finish exercise 1.2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -5,19 +5,31 @@ const Header = (prop) => {
     </>
   );
 };
+
 const Content = (prop) => {
+  return (
+    <>
+      <Part part={prop.part1} exercise={prop.exercise1} />
+      <Part part={prop.part2} exercise={prop.exercise2} />
+      <Part part={prop.part3} exercise={prop.exercise3} />
+    </>
+  );
+};
+
+const Total = (prop) => {
+  return (
+    <>
+      <p>{prop.num1 + prop.num2 + prop.num3} </p>
+    </>
+  );
+};
+
+const Part = (prop) => {
   return (
     <>
       <p>
         {prop.part} {prop.exercise}
       </p>
-    </>
-  );
-};
-const Total = (prop) => {
-  return (
-    <>
-      <p>{prop.num1 + prop.num2 + prop.num3} </p>
     </>
   );
 };
@@ -34,9 +46,14 @@ const App = () => {
   return (
     <div>
       <Header name={course} />
-      <Content part={part1} exercise={exercise1} />
-      <Content part={part2} exercise={exercise2} />
-      <Content part={part3} exercise={exercise3} />
+      <Content
+        part1={part1}
+        exercise1={exercise1}
+        part2={part2}
+        exercise2={exercise2}
+        part3={part3}
+        exercise3={exercise3}
+      />
       <Total num1={exercise1} num2={exercise2} num3={exercise3} />
     </div>
   );
