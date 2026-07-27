73f946d037bd3eb5f83c2a40926c2720865c1af5
Ayub Khoirudin <khoirudinayub971@gmail.com>
2026-07-27 11:26:09 +0700

    add 2 component

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,21 +1,28 @@
 import { useState } from "react";
 
-const Statistics = (props) => {
-  const good = props.good;
-  const bad = props.bad;
-  const neutral = props.neutral;
+const StatisticLine = ({ text, value }) => {
+  return (
+    <>
+      <p>
+        {text} {value}
+      </p>
+    </>
+  );
+};
+
+const Statistics = ({ good, neutral, bad }) => {
   const total = good + bad + neutral;
   const average = (good * 1 + neutral * 0 + bad * -1) / total;
 
-  if (total != 0) {
+  if (total !== 0) {
     return (
       <>
-        <p>good {good} </p>
-        <p>neutral {neutral} </p>
-        <p>bad {bad} </p>
-        <p>all {total} </p>
-        <p>average {average} </p>
-        <p>positive {(good / total) * 100} %</p>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={total} />
+        <StatisticLine text="average" value={average} />
+        <StatisticLine text="positive" value={`${(good / total) * 100} %`} />
       </>
     );
   }
@@ -26,26 +33,34 @@ const Statistics = (props) => {
   );
 };
 
-const Header = (props) => {
+const Header = ({ header }) => {
   return (
     <>
-      <h1>{props.header} </h1>
+      <h1>{header} </h1>
+    </>
+  );
+};
+
+const Button = (props) => {
+  return (
+    <>
+      <button onClick={props.onClick}>{props.text} </button>
     </>
   );
 };
 
 const App = () => {
-  const [good, setGood] = useState(6);
-  const [neutral, setNeutral] = useState(2);
-  const [bad, setBad] = useState(1);
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
   const header = ["give feedback", "statistics"];
 
   return (
     <div>
       <Header header={header[0]} />
-      <button>good</button>
-      <button>neutral</button>
-      <button>bad</button>
+      <Button onClick={() => setGood(good + 1)} text={"good"} />
+      <Button onClick={() => setNeutral(neutral + 1)} text={"neutral"} />
+      <Button onClick={() => setBad(bad + 1)} text={"bad"} />
       <Header header={header[1]} />
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
