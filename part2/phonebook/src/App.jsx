67b807a7be1df7412c8abed151c7d75eaad35485
Ayub Khoirudin <khoirudinayub971@gmail.com>
2026-07-28 18:21:45 +0700

    add phonebook project & almost finish

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import "./App.css";
+
+const ListName = ({ list }) => {
+  return (
+    <li>
+      {list.name} {list.number}{" "}
+    </li>
+  );
+};
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456" },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [search, setSearch] = useState("");
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const nameToShow = search
+    ? persons.filter((person) => person.name.toLowerCase().includes(search))
+    : persons;
+
+  const addPhonebook = (event) => {
+    event.preventDefault();
+    const newNameObject = { name: newName, number: newNumber };
+    const isDuplicate = persons.some(
+      (item) =>
+        item.name.toLowerCase().replace(/\s+/g, "") ===
+        newName.toLowerCase().replace(/\s+/g, ""),
+    );
+    if (!isDuplicate) {
+      setPersons(persons.concat(newNameObject));
+      setNewName("");
+      setNewNumber("");
+    } else {
+      alert(`${newName} is already added to phonebook`);
+      setNewName("");
+      setNewNumber("");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      filter shown with: <input onChange={handleSearchChange} />
+      <h3>add a new</h3>
+      <form onSubmit={addPhonebook}>
+        <div>
+          name: <input onChange={handleNameChange} value={newName} />
+        </div>
+        <div>
+          number: <input onChange={handleNumberChange} value={newNumber} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h3>Numbers</h3>
+      {nameToShow.map((item) => (
+        <ListName key={item.name} list={item} />
+      ))}
+    </div>
+  );
+};
+
+export default App;
---
+++ part2/phonebook/src/main.jsx
@@ -0,0 +1,5 @@
+import ReactDoom from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+ReactDoom.createRoot(document.getElementById("root")).render(<App />);
