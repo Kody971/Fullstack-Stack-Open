df691f9ad40e0f78f20b863271ccb1ca46e7e070
Ayub Khoirudin <khoirudinayub971@gmail.com>
2026-08-13 18:52:23 +0700

    finish exercise 3.2

--- part2/phonebook/backend/index.js
+++ part2/phonebook/backend/index.js
@@ -30,6 +30,12 @@ app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
 
+app.get("/info", (req, res) => {
+  const date = new Date().toString();
+  res.send(`<p>Phonebook has info for ${persons.length} people</p>
+        <p>${date}</p>`);
+});
+
 const PORT = 3001;
 app.listen(PORT);
 console.log("listening on port 3001");
